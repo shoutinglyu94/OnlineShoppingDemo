01b6ad739b91245986a9bab8f81500083aa47241
Shouting Lyu <lyu.s@husky.neu.edu>
2018-09-27 00:07:55 +0800

    Online Shopping Add Ads Application

--- src/main/java/onlineShop/ApplicationConfig.java
+++ src/main/java/onlineShop/ApplicationConfig.java
@@ -1,5 +1,4 @@
 package onlineShop;
-
 import java.util.Properties;
 
 import javax.sql.DataSource;
@@ -12,6 +11,7 @@ import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
 
 @Configuration
 public class ApplicationConfig {
@@ -29,15 +29,13 @@ public class ApplicationConfig {
 	public DataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		
-		// TODO: Change port according to your database settings
-dataSource.setUrl("jdbc:mysql://localhost:3307/laioffer_ecommerce?serverTimezone=UTC");
+		dataSource.setUrl("jdbc:mysql://localhost:3307/laioffer_ecommerce?serverTimezone=UTC");
 		dataSource.setUsername("root");
 		dataSource.setPassword("root");
 
 		return dataSource;
 	}
-	   // Handler for multipart form data
+
     @Bean
     public MultipartResolver multipartResolver() {
    	 CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
@@ -45,7 +43,6 @@ dataSource.setUrl("jdbc:mysql://localhost:3307/laioffer_ecommerce?serverTimezone
    	 return multipartResolver;
     }
     
-    // Properly encode URL encoding for special characters.
     @Bean
     public HttpFirewall allowUrlEncodedSlashHttpFirewall() {
     	StrictHttpFirewall firewall = new StrictHttpFirewall();
@@ -54,12 +51,10 @@ dataSource.setUrl("jdbc:mysql://localhost:3307/laioffer_ecommerce?serverTimezone
     	return firewall;
     }
 
-
 	private final Properties hibernateProperties() {
 		Properties hibernateProperties = new Properties();
-		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop"); // create-drop - > update
 		hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
 		return hibernateProperties;
 	}
 }
-
