f526c18b57301b7c69aaa20ef6a3a1b5cc4b37d8
Shouting Lyu <35321600+shoutinglyu94@users.noreply.github.com>
2019-03-31 20:52:35 -0700

    Update MySQLDBUtil.java

--- AdsApplication/src/db/mysql/MySQLDBUtil.java
+++ AdsApplication/src/db/mysql/MySQLDBUtil.java
@@ -4,8 +4,8 @@ public class MySQLDBUtil {
 	private static final String HOSTNAME = "localhost";
 	private static final String PORT_NUM = "3307"; 
 	public static final String DB_NAME = "laioffer_adsystem";
-	private static final String USERNAME = "root";
-	private static final String PASSWORD = "root";
+	private static final String USERNAME = "";
+	private static final String PASSWORD = "";
 	public static final String URL = "jdbc:mysql://"
 			+ HOSTNAME + ":" + PORT_NUM + "/" + DB_NAME
 			+ "?user=" + USERNAME + "&password=" + PASSWORD
